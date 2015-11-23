d8a52424aa79dff5c80300f460c5283d7e7fb97b
BrickGreen <bagree07@louisville.edu>
2015-11-23 16:38:27 -0500

    added twitter4j and classes tweet and tweet adapter
    I installed the twitter4j library and created path dependencies. Next, I made a class to control a tweet and a tweet adapter to send the information to the view

--- CheckItOut/app/src/main/java/com/example/checkitout/MainActivity.java
+++ CheckItOut/app/src/main/java/com/example/checkitout/MainActivity.java
@@ -1,23 +1,49 @@
 package com.example.checkitout;
 
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ScrollView;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.OAuth2Token;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tweetDisplay;
+    ListView lstMedia;
+    Button btnSubmit;
+    private final String TWIT_CONS_KEY = "0PidwQdIP3Yf1oybRPYLal6A5";
+    private final String TWIT_CONS_SEC_KEY = "q5hxuA2C7vz8FD8ebt5iG0MeoK9ua1puem43t0Ydh8NPyaKp3h";
+    private final String TWIT_TOKEN = "1017676118-HYrdTLTxnWtxc5um9CvooakWknb9PXYIbLxfzeS";
+    private final String TWIT_TOKEN_SEC = "yQStWvg3n8JO7wpBN5kgoQ18cYK2t7x4D5TRzGKXtAXxf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tweetDisplay = (TextView)findViewById(R.id.txtTweets);
+        lstMedia = (ListView) findViewById(R.id.list);
+        btnSubmit = (Button) findViewById(R.id.btnLocation);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
     }
 
     @Override
@@ -45,5 +71,72 @@ public class MainActivity extends AppCompatActivity {
     public void searchTwitter(View view) {
         String searchURL = "https://api.twitter.com/1.1/search/tweets.json?geocode=38.2177972,-85.7628502,15mi";
         tweetDisplay.setText(searchURL);
+    }
+
+    class SearchOnTwitter extends AsyncTask<String, Void, Integer> {
+        ArrayList<Tweet> tweets;
+        final int SUCCESS = 0;
+        final int FAILURE = SUCCESS + 1;
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            try {
+                ConfigurationBuilder builder = new ConfigurationBuilder();
+                builder.setUseSSL(true);
+                builder.setApplicationOnlyAuthEnabled(true);
+                builder.setOAuthConsumerKey(TWIT_CONS_KEY);
+                builder.setOAuthConsumerSecret(TWIT_CONS_SEC_KEY);
+
+                OAuth2Token token = new TwitterFactory(builder.build()).getInstance().getOAuth2Token();
+
+                builder = new ConfigurationBuilder();
+                builder.setUseSSL(true);
+                builder.setApplicationOnlyAuthEnabled(true);
+                builder.setOAuthConsumerKey(TWIT_CONS_KEY);
+                builder.setOAuthConsumerSecret(TWIT_CONS_SEC_KEY);
+                builder.setOAuth2TokenType(token.getTokenType());
+                builder.setOAuth2AccessToken(token.getAccessToken());
+
+                Twitter twitter = new TwitterFactory(builder.build()).getInstance();
+
+                DownloadManager.Query query = new DownloadManager.Query(params[0]);
+                // YOu can set the count of maximum records here
+                query.setCount(50);
+                QueryResult result;
+                result = twitter.search(query);
+                List<twitter4j.Status> tweets = result.getTweets();
+                StringBuilder str = new StringBuilder();
+                if (tweets != null) {
+                    this.tweets = new ArrayList<Tweet>();
+                    for (twitter4j.Status tweet : tweets) {
+                        str.append("@" + tweet.getUser().getScreenName() + " - " + tweet.getText() + "\n");
+                        System.out.println(str);
+                        this.tweets.add(new Tweet("@" + tweet.getUser().getScreenName(), tweet.getText()));
+                    }
+                    return SUCCESS;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return FAILURE;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            dialog.dismiss();
+            if (result == SUCCESS) {
+                list.setAdapter(new TweetAdapter(MainActivity.this, tweets));
+            } else {
+                Toast.makeText(MainActivity.this, getString(R.string.error), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
---
+++ CheckItOut/app/src/main/java/com/example/checkitout/Tweet.java
@@ -0,0 +1,30 @@
+package com.example.checkitout;
+
+/**
+ * Created by aspendavis on 11/22/15.
+ */
+public class Tweet {
+    String tweetBy;
+    String tweet;
+
+    public Tweet(String tweetBy, String tweet) {
+        this.tweetBy = tweetBy;
+        this.tweet = tweet;
+    }
+
+    public String getTweetBy() {
+        return tweetBy;
+    }
+
+    public void setTweetBy(String tweetBy) {
+        this.tweetBy = tweetBy;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
+    }
+}
